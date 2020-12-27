cf5e6191873bbea26b9d115eafa6bf7debc79100
Ikaros76 <didier.rithner@gmail.com>
2020-12-27 13:20:35 +0000

    renamed:    ascii.c -> demoAscii.c

--- demoAscii.c
+++ demoAscii.c
@@ -8,7 +8,6 @@
 
 #include "RA8875.h"
 #include "RA8875Register.h"
-#include "Fonts/FreeSans24pt7b.h"
 
 #include <bcm2835.h>
 
@@ -16,17 +15,8 @@ raspiRA8875 tft = raspiRA8875();
 
 int main(void)
 {
-tft.setFontGFX(&FreeSans24pt7b);
-tft.displaySpiBegin();
 tft.displayBegin();
 tft.displayOn(true);
-for (;;){
-uint16_t x = (rand() % 800)+1;
-uint16_t y = (rand() % 480)+1;
-uint16_t c = (rand() % 0xFFFF)+1;
-tft.drawCharGFX(x,y,"RA8875",c,1);
-}
-delay(4000);
 tft.textMode();
 tft.setTextColor(RA8875_WHITE,RA8875_BLACK);
 for(;;) {
