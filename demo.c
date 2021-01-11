2fee4034009e5c534419fa7b5eccb10ad740740d
Ikaros76 <didier.rithner@gmail.com>
2021-01-11 13:32:01 +0000

    new file:   .demoAscii.c.swp 	modified:   demo.c 	deleted:    demoGFXFont.c 	deleted:    .demoAscii.c.swp

--- demo.c
+++ demo.c
@@ -9,6 +9,9 @@
 #include <cstring>
 
 #include "Fonts/FreeSans24pt7b.h"
+#include "Fonts/comic60pt7b.h"
+#include "Fonts/FreeMonoOblique24pt7b.h"
+#include "Fonts/FreeSerifBold60pt7b.h"
 
 raspiRA8875 tft = raspiRA8875(3, 2);
 
@@ -19,27 +22,29 @@ int main(void) {
   }
   tft.displayOn(true);
   tft.setFontGFX(&FreeSans24pt7b);
+  tft.drawCharGFX(0,40,"RA8875 Raspberry PI",RA8875_GREEN,1);
+  tft.setFontGFX(&comic60pt7b);
+  tft.drawCharGFX(0,160,"Demo RA8875",RA8875_MAGENTA,1);
+  delay(3000);
   tft.fillScreen(RA8875_RED);
   delay(500);
   tft.fillScreen(RA8875_GREEN);
   delay(500);
   tft.fillScreen(RA8875_BLUE);
   delay(500);
-  uint32_t temps = tft.millis();
+  tft.fillScreen(RA8875_BLACK);
+  tft.setFontGFX(&FreeMonoOblique24pt7b);
+  tft.drawCharGFX(350,250,"Line demo",RA8875_WHITE,1);
+  delay(2000);
   for (uint16_t t=0;t<=10000;t++) {
-  int x0 = (rand() % 800)+1;
-  int x1 = (rand() % 800)+1;
-  int y0 = (rand() % 480)+1;
-  int y1 = (rand() % 480)+1;
-  int c = (rand() % 0xFFFF)+1;
-  tft.drawLine(x0,y0,x1,y1,c);
+    int x0 = (rand() % 800)+1;
+    int x1 = (rand() % 800)+1;
+    int y0 = (rand() % 480)+1;
+    int y1 = (rand() % 480)+1;
+    int c = (rand() % 0xFFFF)+1;
+    tft.drawLine(x0,y0,x1,y1,c);
   }
-  float p = (tft.millis()-temps)/1000;
-  printf("%f\n",p);
-  std::string pi = "Pi is " + std::to_string(p);
-  int n = pi.length();
-  char char_array[n+1];
-  strcpy(char_array, pi.c_str());
-  tft.drawCharGFX(100,100,char_array,RA8875_WHITE,1);
-
+  delay(1000);
+  tft.fillScreen(RA8875_BLACK);
+  tft.drawCharGFX(350,250,"Rect demo",RA8875_WHITE,1);
 }
